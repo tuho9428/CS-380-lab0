e2eec5fae6f7f37080f271fefee02d16f6ab8e08
tuho9428 <tu.ho9428@gmail.com>
2023-03-30 18:55:25 -0700

    3rd commit

--- lab0.java
+++ lab0.java
@@ -3,9 +3,17 @@ package test;
 public class lab0 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("First Lab CS 380");
-		System.out.print("Hello");
+	// TODO Auto-generated method stub
+		String str= "12345", nstr="";
+		char ch;
+     
+       
+		for (int i=0; i<str.length(); i++)
+		{
+			ch= str.charAt(i); //extracts each character
+			nstr= ch+nstr; //adds each character in front of the existing string
+		}
+		System.out.println("Reversed: "+ nstr);
 
 	}
 
